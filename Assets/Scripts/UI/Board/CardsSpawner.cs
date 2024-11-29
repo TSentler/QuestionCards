0fa5d1a6c7ef7dc6aa60fb6158f523315a1b20fb
TSentler <centner_dc@mail.ru>
2024-11-29 17:03:05 +0500

    Add board canvas

---
+++ Assets/Scripts/UI/Board/CardsSpawner.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace UI.Board
+{
+    public class CardsSpawner : MonoBehaviour
+    {
+        [SerializeField] private Transform _cardsParent;
+        [SerializeField] private GameObject _cardPrefab;
+    
+        private List<GameObject> _cards = new List<GameObject>();
+
+        private void Awake()
+        {
+            foreach (Transform child in _cardsParent)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+
+        public void Add()
+        {
+            GameObject card = Instantiate(_cardPrefab, _cardsParent);
+            _cards.Add(card);
+        }
+
+        public void Clear()
+        {
+            foreach (GameObject card in _cards)
+            {
+                Destroy(card);
+            }
+            _cards.Clear();
+        }
+    }
+}
